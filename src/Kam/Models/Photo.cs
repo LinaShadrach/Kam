70467b1aff11f97781d5c04b7b1618c4deea7452
Lina Shadrach <lina.shadrach@gmail.com>
2017-09-09 13:48:51 -0700

    Add properties to Photo model

--- src/Kam/Models/Photo.cs
+++ src/Kam/Models/Photo.cs
@@ -7,6 +7,10 @@ namespace Kam.Models
 {
     public class Photo
     {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool Approved { get; set; }
+        public byte[] Image { get; set; }
 
     }
 }
